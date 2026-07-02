2b6a654babf969caa15d88c6317d37a8f2bc5b5d
NstorTapia23 <nestortriana287@gmail.com>
2026-07-02 15:37:41 -0400

    configuracion de neon

--- DB/index.ts
+++ DB/index.ts
@@ -1,6 +1,6 @@
 import { drizzle } from 'drizzle-orm/node-postgres';
 import { Pool } from 'pg';
-import * as schema from "./schema"; // optional
+import * as schema from "./schema"; 
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
